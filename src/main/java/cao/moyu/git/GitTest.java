7168b18af82310329b296480d66801a0ebc59e5d
hxx <haoxx9527@gmail.com>
2022-03-11 15:28:15 +0800

    hot-fix forth commit

--- src/main/java/cao/moyu/git/GitTest.java
+++ src/main/java/cao/moyu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println();
         System.out.println("hot-fix");
         System.out.println("hot-fix  2");
+        System.out.println("hot-fix  3");
     }
 }
