598ae799d8d88bfcd08095f8fcba94ede522ed6e
hxx <haoxx9527@gmail.com>
2022-03-11 15:40:53 +0800

    master-1.0

--- src/main/java/cao/moyu/git/GitTest.java
+++ src/main/java/cao/moyu/git/GitTest.java
@@ -2,11 +2,6 @@ package cao.moyu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("master");
-        System.out.println("master  2");
-        System.out.println("hot-fix");
-        System.out.println("hot-fix  2");
-        System.out.println("hot-fix  3");
+
     }
 }
