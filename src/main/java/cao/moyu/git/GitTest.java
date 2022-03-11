14c415a8f069fbb4b69b2ad72e0ee517ecd6b11c
hxx <haoxx9527@gmail.com>
2022-03-11 15:28:59 +0800

    master forth commit

--- src/main/java/cao/moyu/git/GitTest.java
+++ src/main/java/cao/moyu/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("master");
+        System.out.println("master  2");
         System.out.println("hot-fix");
         System.out.println("hot-fix  2");
     }
