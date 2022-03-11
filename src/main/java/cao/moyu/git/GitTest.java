8a0270bd4ebaaec4dd4b9b2feec67b948dff97ac
hxx <haoxx9527@gmail.com>
2022-03-11 15:46:06 +0800

    hot-fix-1.1

--- src/main/java/cao/moyu/git/GitTest.java
+++ src/main/java/cao/moyu/git/GitTest.java
@@ -2,6 +2,9 @@ package cao.moyu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-
+        System.out.println("======");
+        System.out.println("======");
+        System.out.println("======");
+        System.out.println("hot-fix");
     }
 }
